52b76462d8fe17bbd66af7c5900ee27f2c946158
gleuch <greg@betaworks.com>
2017-10-30 18:47:04 -0400

    you had a lot of typos in your sketch.js file. you need to be careful about names. keywords, variables, and functions are CASE-sensitive.

--- Week-04/Sketch.js
+++ Week-04/Sketch.js
@@ -1,22 +1,18 @@
-var x=320;
-var y=240;
-var width=75;
-var height=75;
+var x = 320;
+var y = 240;
+var width = 75;
+var height = 75;
 
 
 
-Function setup() {
-	createcanvas (640;480);
-
+function setup() {
+  createCanvas(640, 480);
 }
 
 function draw() {
-Background(106,106,146) ;
-
-fill(135,184,153);
-rect(x,y,width,height)
-x=x+1
-y=y+5
-
+  background(106,106,146);
+  fill(135,184,153);
+  rect(x, y, width, height);
+  x = x + 1;
+  y = y + 5;
 }
-
