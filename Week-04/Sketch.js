893f9238274db2019de8c048f7a58074e6a0e36f
alliebsmith <32471243+alliebsmith@users.noreply.github.com>
2017-10-29 21:54:47 -0400

    Week-04
    Moving

---
+++ Week-04/Sketch.js
@@ -0,0 +1,22 @@
+var x=320;
+var y=240;
+var width=75;
+var height=75;
+
+
+
+Function setup() {
+	createcanvas (640;480);
+
+}
+
+function draw() {
+Background(106,106,146) ;
+
+fill(135,184,153);
+rect(x,y,width,height)
+x=x+1
+y=y+5
+
+}
+
