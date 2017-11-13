3b9898e9cb42ed6129a4e54d8cbd871155b2b829
alliebsmith <32471243+alliebsmith@users.noreply.github.com>
2017-11-12 23:05:10 -0500

    Week-07

--- Week-06/Bouncing.js
+++ Week-06/Bouncing.js
@@ -1,31 +1,50 @@
-var x = 5;
-var y = 5;
-var width = 5;
-var height = 5;
-var windowwidth = 1500;
+var ex = 1;
+var ey = 1;
+var ew = 40;
+var eh = 40;
+var rx = 1199;
+var ry = 699;
+var rw = 40;
+var rh = 40;
+var windowwidth = 1450;
 var windowheight = 700;
-var xspeed = 10;
-var yspeed = 7;
+var exspeed = 10;
+var eyspeed = 7;
+var rxspeed = 9;
+var ryspeed = 6;
 
 function setup() {
   createCanvas(windowwidth, windowheight);
 }
 
 function draw() {
-  background (13,142,41);
-	fill (214,195,13)
-  	ellipse (x, y, width, height);
+  background (141,250,178);
+  	noStroke ();
+	fill (106,187,133)
+  	ellipse (ex, ey, ew, eh);
+  	fill (35,62,44)
+  	rect (rx,ry,rw,rh);
 
- 	 x = x + xspeed;
-  	
-  	 if (x > windowwidth | x < 5)  {
-     	xspeed = -xspeed;
+ 	 ex = ex + exspeed;
+  	 rx = rx - rxspeed;
+
+  	 if (ex > windowwidth | ex < 1)  {
+     	exspeed = -exspeed;
   	}
 
-  	y = y + yspeed;
-
-  	if (y > windowheight | y < 5) {
-	 	  yspeed = -yspeed;
+  	 if (rx > windowwidth | rx < 1)  {
+     	rxspeed = -rxspeed;
   	}
+
+  	ey = ey + eyspeed;
+	ry = ry - ryspeed;
+
+  	if (ey > windowheight | ey < 1) {
+	 	  eyspeed = -eyspeed;
+  	}
+
+	if (ry > windowheight | ry < 1) {
+		 	  ryspeed = -ryspeed;
+	}
 
 }
